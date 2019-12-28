86758a7862fb834b1792437b1c26abc2d3f45c37
Torrance Hodgson <torrance123@gmail.com>
2019-12-28 19:22:12 +0800

    Large restructure of entrypoint, especially to introuce multi-processing for both IO reading and during calibration.

--- bin/calibrate.jl
+++ bin/calibrate.jl
@@ -1,8 +1,12 @@
+@everywhere using Pkg
+@everywhere Pkg.activate(".")
 using ArgParse
+using Dates
+using Distributed
 using FileIO
 using JLD
 using Logging
-using MWAjl
+@everywhere using MWAjl
 
 s = ArgParseSettings()
 @add_arg_table s begin
@@ -32,7 +36,7 @@ s = ArgParseSettings()
     "--max-iterations", "-i"
         help="The maximum number of iterations allowed when solving for a single solution unit (ie. for a given channel and time block)."
         arg_type=Int
-        default=100
+        default=50
     "--apply-beam"
         help="The path to the MWA HD5 beam file."
         arg_type=String
@@ -42,6 +46,10 @@ s = ArgParseSettings()
     "--modelcolumn"
         arg_type=String
         default="MODEL_DATA"
+    "--nbatch"
+        help="When reading from `datacolumn` and `modelcolumn`, this parameter specifies how many `chanwidth` arrays to read."
+        arg_type=Int
+        default=1
     "--verbose"
         action=:store_true
     "--debug"
@@ -115,11 +123,39 @@ jones[1, 1, :, :, :] .= 1
 jones[2, 2, :, :, :] .= 1
 @debug "Created Jones array: ", size(jones)
 
-for i in 1:timeblocks
-    t1 = (i - 1) * args["timewidth"] + 1
-    t2 = min(ntimesteps, i * args["timewidth"])
-    @debug "Calibrating timesteps $t1 to $t2"
 
+# Initialise workers
+ch = RemoteChannel(() -> Channel{
+    Tuple{Array{ComplexF32, 4}, Array{ComplexF32, 4}, Vector{Int}, Vector{Int}, Int, Int}
+}(args["nbatch"]))
+futures = Future[]
+for pid in workers()
+    f = remotecall(function (ch, jones, args)
+        try
+            while true
+                elapsed = @elapsed begin
+                    (data, model, ants1, ants2, chanblock, timeblock) = take!(ch)
+                end
+                @info "Reading data (timeblock $timeblock chanblock $chanblock) elapsed $elapsed"
+                elapsed = @elapsed calibrate!(view(jones, :, :, :,  chanblock, timeblock), data, model, similar(data, Float32), ants1, ants2, args["max-iterations"], args["tolerance"]...)
+                @info "Calibration (timeblock $timeblock chanblock $chanblock) elapsed $elapsed"
+            end
+        catch e
+            if isa(e, InvalidStateException)
+                # Channel is closed, time to return our results
+            else
+                # TODO: propagate exception to main thread
+            end
+        end
+        return jones
+    end, pid, ch, jones, args)
+    push!(futures, f)
+end
+
+
+for timeblock in 1:timeblocks
+    t1 = (timeblock - 1) * args["timewidth"] + 1
+    t2 = min(ntimesteps, timeblock * args["timewidth"])
     submset = taql("
         select * from \$1 where 
         ANTENNA1 <> ANTENNA2
@@ -130,45 +166,52 @@ for i in 1:timeblocks
         and TIME <= $(timesteps[t2])
     ", mset)
 
-    @debug "Loading data from submset..."
+    # Load antenna IDs
+    # Add one to satisfy one-indexed convention
     ants1 = column(submset, "ANTENNA1") .+ 1
-    @debug "Loaded ants1 $(size(ants1))"
     ants2 = column(submset, "ANTENNA2") .+ 1
-    @debug "Loaded ants2 $(size(ants2))"
 
-    for j in 1:chanblocks
-        c1 = (j - 1) * args["chanwidth"] + 1
-        c2 = min(nchans, j * args["chanwidth"])
-        @debug "Calibrating channel range $c1 to $c2"
+    chanblock = 1
+    batchsize = args["chanwidth"] * args["nbatch"]
+    for batchstart in 1:batchsize:nchans
+        batchend = min(nchans, batchstart + batchsize - 1)
 
-        # Pre-seed previous Jones solution
-        if j > 1
-            jones[:, :, :, j, i] .= jones[:, :, :, j-1, i]
-            replace!(view(jones, 1, 1, :, j, i), NaN=>1)
-            replace!(view(jones, 1, 2, :, j, i), NaN=>0)
-            replace!(view(jones, 2, 1, :, j, i), NaN=>0)
-            replace!(view(jones, 2, 2, :, j, i), NaN=>1)
+        # Read in data from measurement set
+        @debug "Fetching new batch of data"
+        elapsed = @elapsed begin
+            data = column(submset, "DATA", blc=[1, batchstart], trc=[4, batchend])
+            model = column(submset, "MODEL_DATA", blc=[1, batchstart], trc=[4, batchend])
         end
+        @debug "Finished fetching new batch of data, elapsed $elapsed"
 
-        data = column(submset, "DATA", blc=[1, c1], trc=[4, c2])
-        @debug "Loaded data $(size(data))"
-        model = column(submset, "MODEL_DATA", blc=[1, c1], trc=[4, c2])
-        @debug "Loaded model $(size(model))"
-
+        # Reshape so that Jones matrices from (4,) to (2, 2), as expected by calibrate
         data = reshape(data, 2, 2, size(data)[2:end]...)
         model = reshape(model, 2, 2, size(model)[2:end]...)
 
-        calibrate!(view(jones, :, :, :,  j, i), data, model, similar(data, Float32), ants1, ants2, args["tolerance"]...)
+        # Send data to workers
+        for chstart in 1:args["chanwidth"]:(batchend - batchstart + 1)
+            chend = chstart + args["chanwidth"] - 1
+            put!(ch, (data[:, :, chstart:chend, :], model[:, :, chstart:chend, :], ants1, ants2, chanblock, timeblock))
+            chanblock += 1
+        end
+        data = nothing
+        model = nothing
     end
+end
+
+# Wait on workers to complete and combine final jones matrix
+for f in futures
+    jones .+= fetch(f)
 end
 
 # Invert jones Matrices so that they can be applied as J D J^H
 for timeblock in axes(jones[5])
     for chanblock in axes(jones[4])
         for antid in axes(jones[3])
-            jones[:, :, antid, chanblock, timeblock] = inv(jones[:, :, antid, chanblock, timeblock])
+            jones[:, :, antid, chanblock, timeblock] .= inv(jones[:, :, antid, chanblock, timeblock])
         end
     end
 end
 
+@info "Total elapsed: $(duration_read.value / 1000)s (reading) $(duration_calibration.value / 1000)s (calibration)"
 save(File(format"JLD", args["solution"]), "options", args, "jones", jones)
