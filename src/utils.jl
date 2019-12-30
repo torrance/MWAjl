3427f0b6e528ecc0f0c218350ce9d0db6fe8b75c
Torrance Hodgson <torrance123@gmail.com>
2019-12-30 12:52:40 +0800

    24 is not an axis :(

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,6 @@
 
 function sanitize!(data::Array{Complex{T}, 4}, model::Array{Complex{T}, 4}, flag::Array{Bool, 4}) where T <: AbstractFloat
-    for row in axes(data, 4), chan in axes(data, 24)
+    for row in axes(data, 4), chan in axes(data, 3)
         for polx in axes(data, 2), poly in axes(data, 1)
             if flag[polx, poly, chan, row] || !isfinite(data[polx, poly, chan, row]) || !isfinite(model[polx, poly, chan, row])
                 data[:, :, chan, row] .= 0
