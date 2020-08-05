2a998e0ed9a7ab2084d8da4804aae002c3737bb6
Torrance Hodgson <torrance123@gmail.com>
2020-08-05 10:09:37 +0800

    Remove GPU sysimage compilation and MWAjl package (refs #6)

--- scripts/makesysimage.jl
+++ scripts/makesysimage.jl
@@ -16,7 +16,9 @@ precompile = joinpath(temp_dir, "precompile.jl")
 
 run(`julia --trace-compile=$cpu_precompile --project=$project $calibrate --model $testdata/data/model.txt --apply-beam -a 1E-5 1E-6 --verbose $testdata/data/1217094992-ch8-19.ms /dev/null`)
 
-run(`julia --trace-compile=$gpu_precompile --project=$project $calibrate --model $testdata/data/model.txt --apply-beam -a 1E-5 1E-6 --gpu --verbose $testdata/data/1217094992-ch8-19.ms /dev/null`)
+# Currently, CUDA drivers are not available during Singulairty build processes.
+# run(`julia --trace-compile=$gpu_precompile --project=$project $calibrate --model $testdata/data/model.txt --apply-beam -a 1E-5 1E-6 --gpu --verbose $testdata/data/1217094992-ch8-19.ms /dev/null`)
+run(`touch $gpu_precompile`)
 
 open(precompile, "w") do io
     write(
@@ -25,6 +27,7 @@ open(precompile, "w") do io
     )
 end
 
+# Remove MWAjl as for some reason this causes the sysimage to segfault. This needs to be fixed.
 PackageCompiler.create_sysimage([
-    :MWAjl, :ArgParse, :CUDAdrv, :CUDAnative, :CuArrays, :HDF5, :LinearAlgebra, :Pkg, :StaticArrays, :Statistics
+    :ArgParse, :CUDAdrv, :CUDAnative, :CuArrays, :HDF5, :LinearAlgebra, :Pkg, :StaticArrays, :Statistics
 ], sysimage_path=joinpath(project, "libs/MWAjl.so"), precompile_statements_file=precompile)
